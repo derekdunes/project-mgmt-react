4559cf704892a76cd7f8e5b0b0a70628395e3d33
mbah derek <mbahderek@gmail.com>
2025-11-25 13:01:52 +0000

    SideBar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import SideBar from "./Components/SideBar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar />
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      </menu>
+    </main>
   );
 }
 
