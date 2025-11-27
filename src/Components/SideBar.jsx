a1e8d0cdc3457b4537dbbdebba2efe8546ef3fbd
mbah derek <mbahderek@gmail.com>
2025-11-27 05:21:04 +0000

    Project List completion

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -8,6 +8,21 @@ function SideBar() {
       <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
         + Add Project
       </button>
+
+
+      <ul className="mt-8">
+        <li>
+          <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+            Learning React
+          </button>
+        </li>
+        <li>
+          <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+            Mastering React
+          </button>
+        </li>
+      </ul>
+
     </aside>
   );
 }
